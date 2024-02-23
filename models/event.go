e0348023007530ea671bb7c3e444146aafa769c5
SnehilSundriyal <snehilsundriyal@gmail.com>
2024-02-23 15:34:51 +0530

    same issue again

--- models/event.go
+++ models/event.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"time"
+	"example.com/REST-API/db"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string `binding:"required"`
 	Description string `binding:"required"`
 	Location    string `binding:"required"`
@@ -13,11 +16,48 @@ type Event struct {
 
 var events []Event = []Event{}
 
-func (e Event) Save() {
-	// later: add it to a database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)
+	`
+
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID) // if we have to change data in database, we use Exec 
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId() // to get id of the event that was inserted as the table was set to automatically set id
+	e.ID = id
+
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := "SELECT * FROM events"
+	rows, err := db.DB.Query(query) // if we have to fetch data, we use Query and not Exec. We are fetching the rows from the events table here.
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() { // Next returns bool as true as long as there is rows left
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+	
+	return events, nil
 }

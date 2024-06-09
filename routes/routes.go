f8a5080d85dc7b0e1f96bd6c7124c3f0ed3d8546
SnehilSundriyal <snehilsundriyal@gmail.com>
2024-06-09 20:21:19 +0530

    update-added

--- routes/routes.go
+++ routes/routes.go
@@ -8,4 +8,5 @@ func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents) // we are setting up a handler for an incoming GET request
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
+	server.PUT("/events/:id", updateEvent)
 }
